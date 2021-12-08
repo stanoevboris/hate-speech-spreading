0823b5df30bab3fffe25a75a9df142beb82d8357
Boris Stanoev <stanoevboris98@gmail.com>
2021-12-08 11:19:37 +0100

    modified functions to handle english and spanish data

--- scripts/data_prepare.py
+++ scripts/data_prepare.py
@@ -4,16 +4,37 @@ import csv
 from scripts.data_preprocessing import preprocess_tweet
 from nltk.tokenize import TweetTokenizer
 
-TWEETS_HS_DATA = \
+# english data
+TWEETS_HS_DATA_EN = \
     'data/profiling-hate-speech-spreaders-twitter/pan21-author-profiling-training-2021-03-14/en/tweets-hs-spreaders.csv'
-TWEETS_CSV = 'data/tweets.csv'
-USERS_CSV = 'data/users.csv'
-WORDS_CSV = 'data/words.csv'
-USERS_DICT = 'data/profiling-hate-speech-spreaders-twitter/pan21-author-profiling-training-2021-03-14/en/truth.txt'
+TWEETS_CSV_EN = 'data/tweets_en.csv'
+USERS_CSV_EN = 'data/users_en.csv'
+WORDS_CSV_EN = 'data/words_en.csv'
+USERS_DICT_EN = 'data/profiling-hate-speech-spreaders-twitter/pan21-author-profiling-training-2021-03-14/en/truth.txt'
+
+# spanish data
+TWEETS_HS_DATA_ES = \
+    'data/profiling-hate-speech-spreaders-twitter/pan21-author-profiling-training-2021-03-14/es/tweets-hs-spreaders.csv'
+USERS_CSV_ES = 'data/users_es.csv'
+TWEETS_CSV_ES = 'data/tweets_es.csv'
+WORDS_CSV_ES = 'data/words_es.csv'
+USERS_DICT_ES = 'data/profiling-hate-speech-spreaders-twitter/pan21-author-profiling-training-2021-03-14/es/truth.txt'
+
+# edges
+USER_TWEETS_EDGES_EN = 'data/users-written-tweets_en.csv'
+USER_TWEETS_EDGES_ES = 'data/users-written-tweets_es.csv'
+
+TWEETS_WORDS_EDGES_EN = 'data/tweet-contains-words_en.csv'
+TWEETS_WORDS_EDGES_ES = 'data/tweet-contains-words_es.csv'
 
 
-def load_user_dict(filepath: str):
+def load_user_dict(language='en'):
     # find each user given
+    if language == 'en':
+        USERS_DICT = USERS_DICT_EN
+    else:
+        USERS_DICT = USERS_DICT_ES
+
     users_dict = {}
     with open(USERS_DICT, encoding='utf-8') as f:
         for line in f.readlines():
@@ -22,7 +43,12 @@ def load_user_dict(filepath: str):
     return users_dict
 
 
-def load_user_mappings():
+def load_user_mappings(language='en'):
+    if language == 'en':
+        USERS_CSV = USERS_CSV_EN
+    else:
+        USERS_CSV = USERS_CSV_ES
+
     mapped_users = {}
     users_df = pd.read_csv(USERS_CSV)
 
@@ -34,8 +60,16 @@ def load_user_mappings():
     return mapped_users
 
 
-def create_user_mappings(filepath: str, users_ids: list, users_dict: dict):
+def create_user_mappings(language='en'):
     # create csv file with users information about their real ids and labels
+    if language == 'en':
+        filepath = USERS_CSV_EN
+    else:
+        filepath = USERS_CSV_ES
+
+    users_dict = load_user_dict(language)
+    users_ids = list(users_dict.keys())
+
     with open(filepath, 'w') as users_csv:
         writer = csv.writer(users_csv, delimiter=",")
         writer.writerow(('ID', 'USER_ID', 'LABEL'))
@@ -44,10 +78,17 @@ def create_user_mappings(filepath: str, users_ids: list, users_dict: dict):
             writer.writerow((index, user, label))
 
 
-def create_tweets_csv(dst_filepath: str):
+def create_tweets_csv(language='en'):
+    if language == 'en':
+        filepath = TWEETS_CSV_EN
+        TWEETS_HS_DATA = TWEETS_HS_DATA_EN
+    else:
+        filepath = TWEETS_CSV_ES
+        TWEETS_HS_DATA = TWEETS_HS_DATA_ES
+
     df = pd.read_csv(TWEETS_HS_DATA)
-    mapped_users = load_user_mappings()
-    with open(dst_filepath, 'w', encoding='utf-8') as twitters_csv:
+    mapped_users = load_user_mappings(language)
+    with open(filepath, 'w', encoding='utf-8') as twitters_csv:
         writer = csv.writer(twitters_csv, delimiter=",")
         writer.writerow(('ID', 'USER_ID', 'RAW_TWEET', 'PREPROCESSED'))
         tweet_id = 0
@@ -72,11 +113,18 @@ def extract_all_words(tweets_df: pd.DataFrame):
     return words
 
 
-def create_word_mappings(dst_filepath: str):
+def create_word_mappings(language='en'):
+    if language == 'en':
+        TWEETS_CSV = TWEETS_CSV_EN
+        filepath = WORDS_CSV_EN
+    else:
+        TWEETS_CSV = TWEETS_CSV_ES
+        filepath = WORDS_CSV_ES
+
     tweets_df = pd.read_csv(TWEETS_CSV)
     words = extract_all_words(tweets_df)
 
-    with open(dst_filepath, 'w', encoding='utf-8') as words_csv:
+    with open(filepath, 'w', encoding='utf-8') as words_csv:
         writer = csv.writer(words_csv, delimiter=",")
         writer.writerow(('ID', 'WORD'))
         word_id = 0
@@ -85,25 +133,41 @@ def create_word_mappings(dst_filepath: str):
             word_id = word_id + 1
 
 
-def generate_users_tweets_edges(dst_filepath: str):
+def generate_users_tweets_edges(language='en'):
+    if language == 'en':
+        TWEETS_CSV = TWEETS_CSV_EN
+        filepath = USER_TWEETS_EDGES_EN
+    else:
+        TWEETS_CSV = TWEETS_CSV_ES
+        filepath = USER_TWEETS_EDGES_ES
+
     tweets_df = pd.read_csv(TWEETS_CSV)
     users = tweets_df['USER_ID'].tolist()
     tweets = tweets_df['ID'].tolist()
 
-    with open(dst_filepath, 'w', encoding='utf-8') as edges_csv:
+    with open(filepath, 'w', encoding='utf-8') as edges_csv:
         writer = csv.writer(edges_csv, delimiter=',')
         writer.writerow(('USER_ID', 'TWEET_ID'))
         for user, tweet in zip(users, tweets):
             writer.writerow((user, tweet))
 
 
-def generate_tweets_words_edges(dst_filepath: str):
+def generate_tweets_words_edges(language='en'):
+    if language == 'en':
+        TWEETS_CSV = TWEETS_CSV_EN
+        WORDS_CSV = WORDS_CSV_EN
+        filepath = TWEETS_WORDS_EDGES_EN
+    else:
+        TWEETS_CSV = TWEETS_CSV_ES
+        WORDS_CSV = WORDS_CSV_ES
+        filepath = TWEETS_WORDS_EDGES_ES
+
     tokenizer = TweetTokenizer()
 
     tweets_df = pd.read_csv(TWEETS_CSV, usecols=['ID', 'PREPROCESSED'], header=0)
     words_df = pd.read_csv(WORDS_CSV)
     words_dict = dict(words_df[['WORD', 'ID']].values)
-    with open(dst_filepath, 'w', encoding='utf-8') as edges_csv:
+    with open(filepath, 'w', encoding='utf-8') as edges_csv:
         writer = csv.writer(edges_csv, delimiter=',')
         writer.writerow(('TWEET_ID', 'WORD_ID'))
 
@@ -117,7 +181,14 @@ def generate_tweets_words_edges(dst_filepath: str):
                 writer.writerow((tweet_id, word_id))
 
 
-def aggregate_tweets_on_user_level() -> pandas.DataFrame:
+def aggregate_tweets_on_user_level(language='en') -> pandas.DataFrame:
+    if language == 'en':
+        USERS_CSV = USERS_CSV_EN
+        TWEETS_CSV = TWEETS_CSV_EN
+    else:
+        USERS_CSV = USERS_CSV_ES
+        TWEETS_CSV = TWEETS_CSV_ES
+
     tweets_df = pd.read_csv(TWEETS_CSV)
     users_df = pd.read_csv(USERS_CSV)
 
